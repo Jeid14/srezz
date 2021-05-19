feafa73da7bd04accc46bf07eee65662eb00ddf4
jeid <jeid14@gmail.com>
2021-05-19 11:52:44 +0300

    add DTO

--- srezzz/src/main/java/com/srezz/controllers/UpdateController.java
+++ srezzz/src/main/java/com/srezz/controllers/UpdateController.java
@@ -1,8 +1,8 @@
 package com.srezz.controllers;
 
-import com.srezz.modelDto.MusicGroupDto;
+import com.srezz.modelDto.MusicGroupUpdateDto;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +12,7 @@ import javax.validation.Valid;
 public class UpdateController {
 
     @PatchMapping("/updateGroup")
-    public void updateMusicGroup(@Valid @RequestBody MusicGroupDto musicGroupDto,){
+    public void updateMusicGroup(@Valid @RequestBody MusicGroupUpdateDto musicGroupUpdateDto, BindingResult bindingResult){
 
     }
 }
--- srezzz/src/main/java/com/srezz/modelDto/MusicGroupUpdateDto.java
+++ srezzz/src/main/java/com/srezz/modelDto/MusicGroupUpdateDto.java
@@ -8,16 +8,16 @@ import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Value
-public class MusicGroupDto {
+public class MusicGroupUpdateDto {
     @NotBlank(message = "Invalid name!")
     String oldName;
-    @Min(value = 1800, message = "Year creation must be no less than 1800")
+    String newName;
+//    @Min(value = 1800, message = "Year creation must be no less than 1800")
     @Max(value = 2022, message = "Year creation must be less than 2022")
     int creationYear;
-    @Min(value = 1800, message = "Year decay must be no less than 1800")
+//    @Min(value = 1800, message = "Year decay must be no less than 1800")
     @Max(value = 2022, message = "Year decay must be less than 2022")
     int decayYear;
-    @Min(value = 0, message = "Available albums must be no less 0")
+//    @Min(value = 0, message = "Available albums must be no less 0")
     int availableAlbums;
-    List<String> genres;
 }
