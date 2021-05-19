c93c6baf9fa2f3cfb5a606eb5ce63f07cec60519
jeid <jeid14@gmail.com>
2021-05-19 11:18:29 +0300

    DTO

--- srezzz/src/main/java/com/srezz/controllers/UpdateController.java
+++ srezzz/src/main/java/com/srezz/controllers/UpdateController.java
@@ -1,14 +1,18 @@
 package com.srezz.controllers;
 
+import com.srezz.modelDto.MusicGroupDto;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class UpdateController {
 
     @PatchMapping("/updateGroup")
-    public void updateMusicGroup(){
+    public void updateMusicGroup(@Valid @RequestBody MusicGroupDto musicGroupDto,){
 
     }
 }
